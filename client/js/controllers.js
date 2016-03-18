1b89f5e7c140b2fdea27a23e4f5440e626151d7a
beeva-eduardolopez <eduardo.lopez.anta@bbva.com>
2016-03-18 09:14:54 -0500

    first steps

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -1,5 +1,98 @@
-//Logic for page actions here. 
+var controllers = angular.module('myBlogApp.controllers', []);
+controllers.controller('NewPostController', ['$rootScope', '$scope', '$http', function($rootScope, $scope, $http) {
+    // $scope.welcomeMessage = 'Hello World!';
+    // $scope.weather = "It's sunny and 85 degrees outside!";
 
-var controllers = angular.module('', []);
+    // $scope.pluckFirstLetter = function() {
+    //     $scope.weather = $scope.weather.substring(1);
+    //     logValue($scope.weather);
+    // }
 
-controllers.controller('', []);
+    // $scope.getGoogle = function() {
+    //     $http({
+    //         url: 'http://depotu.io',
+    //         method: 'GET'
+    //     }).then(function(success) {
+    //         console.log(success);
+    //     }, function(error) {
+    //         console.error(error);
+    //     });
+    // }
+
+    // $scope.sayHelloToEveryone = function() {
+    //     WordFactory.sayHello();
+    //     WordService.sayHello($scope.name);
+    // }
+
+    // $scope.getNote = function() {
+    //     var someNumber = $scope.noteid;
+    //     NoteFactory.get({id: someNumber});
+    // }
+
+    // $scope.getAllTweets = function() {
+    //     NoteFactory.query().then(function(tweets) {
+    //         $scope.tweets = tweets;
+    //     }, console.error);
+    // }
+
+    // function logValue(val) {
+    //     console.log(val);
+    // }
+}]);
+
+controllers.controller('PostPageController', ['$scope', '$rootScope', '$http', , function($scope, $rootScope, $http) {
+
+
+    // var user = $rootScope.user;
+    // $scope.getData = function() {
+    //     $http({
+    //         url: '/api/post',
+    //         method: 'GET'
+    //     }).then(function(result) {
+    //         $scope.posts = result.data;
+    //     }, function(err) {
+    //         console.log(err);
+    //     });
+    // };
+
+    // $scope.instructors = [
+    //     {
+    //         name: 'David',
+    //         email: 'someEmail@depotu.io',
+    //         hobbies: 'Programming'
+    //     },
+    //     {
+    //         name: 'Tyler',
+    //         email: 'anotherEmail@depotu.io',
+    //         hobbies: 'Reading'
+    //     },
+    //     {
+    //         name: 'Hillary',
+    //         email: 'email@depotu.io',
+    //         hobbies: 'Hiking'
+    //     }
+    // ]
+}]);
+
+// controllers.controller('InstructorDetailController', ['$scope', '$routeParams', function($scope, $routeParams) {
+//     var instructorid = $routeParams.id;
+//     var instructors = [
+//         {
+//             name: 'David',
+//             email: 'someEmail@depotu.io',
+//             hobbies: 'Programming'
+//         },
+//         {
+//             name: 'Tyler',
+//             email: 'anotherEmail@depotu.io',
+//             hobbies: 'Reading'
+//         },
+//         {
+//             name: 'Hillary',
+//             email: 'email@depotu.io',
+//             hobbies: 'Hiking'
+//         }
+//     ];
+
+//     $scope.instructor = instructors[instructorid];
+// }]);
--- client/js/main.js
+++ client/js/main.js
@@ -1,5 +1,21 @@
 //Use to instantiate app, connect factory & controllers and configure app.
+var app = angular.module('myBlogApp', ['myBlogApp.controllers', 'myBlogApp.factories', 'myBlogApp.services', 'ngRoute', 'ngResource']);
 
-var app = angular.module('', []);
-
-app.config([]);
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+    .when('/', {
+        templateUrl: 'views/blogposts.html',
+        controller: 'PostPageController'
+    })
+    .when('/newpost', {
+       templateUrl: 'views/newpost.html',
+        controller: 'NewPostController'
+    })
+    // .when('/post/:id', {
+    //     templateUrl: 'views/post.html',
+    //     controller: 'InstructorDetailController'
+    // })
+    .otherwise({
+        redirectTo: '/'
+    });
+}]);
